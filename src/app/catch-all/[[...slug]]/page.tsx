a05a906f2b988414130001ae20a754439040d220
Abhishek Shrestha <meabhi224@gmail.com>
2024-03-08 16:07:14 +0545

    added catch all segments route

---
+++ src/app/catch-all/[[...slug]]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Page({ params }: { params: { slug: string[] } }) {
+  if (params.slug?.length === 2)
+    return (
+      <div>
+        Route - {params.slug[0]} - Sub Route - {params.slug[1]}
+      </div>
+    );
+
+  if (params.slug?.length === 1)
+    return <div>Content for Route - {params.slug[0]}</div>;
+
+  return <div>Default page for all other routes</div>;
+}
