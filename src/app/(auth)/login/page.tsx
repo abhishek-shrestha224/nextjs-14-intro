7c1f296f315083f3ea226ad6fbd7c331de7fa955
Abhishek Shrestha <meabhi224@gmail.com>
2024-03-08 16:42:32 +0545

    added route group auth

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="h-screen w-screen font-primary font-black bg-slate-200 text-slate-800 text-7xl flex justify-center items-center">
+      Login Page
+    </div>
+  );
+}
