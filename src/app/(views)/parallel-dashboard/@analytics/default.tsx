e336e51fc99bc65271348070ebc45dc548a97d90
Abhishek Shrestha <meabhi224@gmail.com>
2024-03-10 13:09:44 +0545

    added default pages for fallback routes

---
+++ src/app/(views)/parallel-dashboard/@analytics/default.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DefaultAnalytics() {
+  return (
+    <div className="border-2 h-full bg-blue-200 text-slate-800 flex justify-center items-center font-bold text-3xl flex-1 rounded-md">
+      Analytics Fallback
+    </div>
+  );
+}
---
+++ src/app/(views)/parallel-dashboard/default.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DefaultDashboard() {
+  return (
+    <div className="w-full h-28 bg-green-400 text-slate-800 flex justify-center items-center font-bold text-4xl rounded-md">
+      Dashboard Fallback
+    </div>
+  );
+}
