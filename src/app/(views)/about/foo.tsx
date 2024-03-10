e57533f113398365b1e1bb556d15f08936c5b489
Abhishek Shrestha <meabhi224@gmail.com>
2024-03-10 08:07:02 +0545

    added libs folder

--- src/app/(views)/about/foo.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Foo() {
-  await wait(2000);
-  return <div>Hello</div>;
-}
-
-function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
--- src/app/(views)/about/page.tsx
+++ src/app/(views)/about/page.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import Foo from "./foo";
+import wait from "@/app/lib/wait";
 import Loading from "./Loader";
 import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "About",
+};
+
+const Foo = async () => {
+  await wait(2000);
+  return <div>Hello</div>;
 };
 
 export default function About() {
